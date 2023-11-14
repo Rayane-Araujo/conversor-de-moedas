376961f35effae7d30192483eabe9819e9e7e072
Rayane Araújo <rayanearaujo28@hotmail.com>
2023-11-14 13:59:16 -0400

    inicial commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+var valorEmDolar = 40
+var cotacaoDoDolar = 5.32
+
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar
+valorEmReal = valorEmReal.toFixed(2)
+
+alert('Olá, :)' + ' o valor convertido para real é: ' + "R$ " + valorEmReal)
